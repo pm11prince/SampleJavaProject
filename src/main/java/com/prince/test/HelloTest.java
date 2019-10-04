10971bfd838374bbb546f467d3591dbff2a353e3
pm11prince <pm11prince@gmail.com>
2019-10-04 12:44:52 +0530

    Update HelloTest.java

--- src/main/java/com/prince/test/HelloTest.java
+++ src/main/java/com/prince/test/HelloTest.java
@@ -4,7 +4,7 @@ public class HelloTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Test");
-		System.out.println("Hello Test New");
+		System.out.println("Hello Test New1");
 		
 	}
 
